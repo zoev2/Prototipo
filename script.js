a6cb87c094a45465022cadd4e8b7cb690bb8753d
zoev2 <narutokurusaki1234@gmail.com>
2025-09-13 11:47:57 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  document.getElementById("login").classList.remove("active");
+  document.getElementById("calendar").classList.add("active");
+});
+
+function showPending() {
+  document.getElementById("tasks-container").innerHTML = `
+    <div class="task-card red">4 AGOSTO 2025</div>
+    <div class="task-card red">7 AGOSTO 2025</div>
+    <div class="task-card red">15 AGOSTO 2025</div>
+  `;
+}
+
+function showCompleted() {
+  document.getElementById("tasks-container").innerHTML = `
+    <div class="task-card green">28 JUNIO 2025</div>
+  `;
+}
